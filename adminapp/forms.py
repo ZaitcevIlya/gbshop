207188f77347f51bb4b88ade03a88fc205088779
ZaitcevIlya <izaytsev.work@gmail.com>
2021-12-19 16:26:45 +0300

    solved lesson 8

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm
 
 from users.models import User
-from mainapp.models import ProductCategory
+from mainapp.models import ProductCategory, Product
 
 
 class UserEditAdminForm(UserChangeForm):
@@ -22,6 +22,18 @@ class UserEditAdminForm(UserChangeForm):
 class ProductCategoryEditForm(forms.ModelForm):
     class Meta:
         model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class ProductEditForm(forms.ModelForm):
+    class Meta:
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
